4fc84ae662eee21d275f33c6f84810557b580142
crazymedguy <crazymedguy@gmail.com>
2020-07-02 12:16:30 +0800

    new change

--- SRS.py
+++ SRS.py
@@ -92,7 +92,7 @@ def get_rtime(top, qs):
     rtime_list = []
     for q in qs:
         timeNow = UTCtoGMT8(datetime.now())
-        time = NotionDate(start=timeNow,end=None,timezone=None)
+        time = NotionDate(start=timeNow,end=None,timezone=timezone('Asia/Singapore'))
         rtime_list.append(time)
     return rtime_list
 
@@ -160,7 +160,7 @@ def UTCtoGMT8(dt):
 def update_topic_date(topic):
     print("Updating topic date...")
     timeNow = UTCtoGMT8(datetime.now())
-    topic.Revised = NotionDate(start=timeNow,end=None,timezone=None)
+    topic.Revised = NotionDate(start=timeNow,end=None,timezone=timezone('Asia/Singapore'))
     return
 
 def update_topic_count(topic):
